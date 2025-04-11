b227009f4b942a67d2c1bc9b552bb87403254cea
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2025-04-11 13:44:40 -0400

    replaced imperative with declarative code
    imperative: how to do it
    declarative: what I want to do, typically easier for human reading, but one-liners like this are typically not that lol

--- stats.py
+++ stats.py
@@ -1,12 +1,8 @@
+from string import ascii_lowercase as letters
+
 def get_num_words(text: str) -> int:
     return text.split().__len__()
 
 def get_char_dict(text: str) -> dict:
-    chars = dict()
-    for c in text.lower():
-        if c in chars:
-            chars[c] += 1
-        else:
-            chars[c] = 1
-    return chars
-
+    lower_text = text.lower()
+    return {letter : lower_text.count(letter) for letter in set(lower_text)}
